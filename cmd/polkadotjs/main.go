8db38c11a14c98dc43e2176a9df71b420d0437da
Gary <62193371+gary111868@users.noreply.github.com>
2025-08-03 01:58:06 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/polkadotjs/main.go
@@ -0,0 +1,23 @@
+// cmd/polkadotjs/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"polkadotjs/internal/polkadotjs"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := polkadotjs.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
